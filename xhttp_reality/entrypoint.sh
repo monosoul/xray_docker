bae214faee82cdd6fe40fcb4b155d2376ff09415
Andrei Nevedomskii <kloz.klaud@gmail.com>
2025-09-22 16:10:54 +0200

    Store configs in a subdir (#1)

--- xhttp_reality/entrypoint.sh
+++ xhttp_reality/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ -f /config_info.txt ]; then
+if [ -f /config/config_info.txt ]; then
   echo "config.json exist"
 else
   IPV6=$(curl -6 -sSL --connect-timeout 3 --retry 2  ip.sb || echo "null")
@@ -24,7 +24,7 @@ else
 
   if [ -n "$HOSTMODE_PORT" ];then
     EXTERNAL_PORT=$HOSTMODE_PORT
-    jq ".inbounds[1].port=$HOSTMODE_PORT" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+    jq ".inbounds[1].port=$HOSTMODE_PORT" /config/config.json >/config/config.json_tmp && mv /config/config.json_tmp /config/config.json
   fi
 
   if [ -z "$DEST" ]; then
@@ -52,51 +52,51 @@ else
   fi
 
   # change config
-  jq ".inbounds[1].settings.clients[0].id=\"$UUID\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
-  jq ".inbounds[1].streamSettings.realitySettings.dest=\"$DEST\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
-  jq ".inbounds[1].streamSettings.xhttpSettings.path=\"$XHTTP_PATH\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+  jq ".inbounds[1].settings.clients[0].id=\"$UUID\"" /config/config.json >/config/config.json_tmp && mv /config/config.json_tmp /config/config.json
+  jq ".inbounds[1].streamSettings.realitySettings.dest=\"$DEST\"" /config/config.json >/config/config.json_tmp && mv /config/config.json_tmp /config/config.json
+  jq ".inbounds[1].streamSettings.xhttpSettings.path=\"$XHTTP_PATH\"" /config/config.json >/config/config.json_tmp && mv /config/config.json_tmp /config/config.json
 
   SERVERNAMES_JSON_ARRAY="$(echo "[$(echo $SERVERNAMES | awk '{for(i=1;i<=NF;i++) printf "\"%s\",", $i}' | sed 's/,$//')]")"
-  jq --argjson serverNames "$SERVERNAMES_JSON_ARRAY" '.inbounds[1].streamSettings.realitySettings.serverNames = $serverNames' /config.json >/config.json_tmp && mv /config.json_tmp /config.json
-  # jq --argjson serverNames "$SERVERNAMES_JSON_ARRAY" '.routing.rules[0].domain = $serverNames' /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+  jq --argjson serverNames "$SERVERNAMES_JSON_ARRAY" '.inbounds[1].streamSettings.realitySettings.serverNames = $serverNames' /config/config.json >/config/config.json_tmp && mv /config/config.json_tmp /config/config.json
+  # jq --argjson serverNames "$SERVERNAMES_JSON_ARRAY" '.routing.rules[0].domain = $serverNames' /config/config.json >/config/config.json_tmp && mv /config/config.json_tmp /config/config.json
 
-  jq ".inbounds[1].streamSettings.realitySettings.privateKey=\"$PRIVATEKEY\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
-  jq ".inbounds[1].streamSettings.network=\"$NETWORK\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+  jq ".inbounds[1].streamSettings.realitySettings.privateKey=\"$PRIVATEKEY\"" /config/config.json >/config/config.json_tmp && mv /config/config.json_tmp /config/config.json
+  jq ".inbounds[1].streamSettings.network=\"$NETWORK\"" /config/config.json >/config/config.json_tmp && mv /config/config.json_tmp /config/config.json
 
 
 
   FIRST_SERVERNAME=$(echo $SERVERNAMES | awk '{print $1}')
   # config info with green color
-  echo -e "\033[32m" >/config_info.txt
-  echo "IPV6: $IPV6" >>/config_info.txt
-  echo "IPV4: $IPV4" >>/config_info.txt
-  echo "UUID: $UUID" >>/config_info.txt
-  echo "DEST: $DEST" >>/config_info.txt
-  echo "PORT: $EXTERNAL_PORT" >>/config_info.txt
-  echo "SERVERNAMES: $SERVERNAMES (任选其一)" >>/config_info.txt
-  echo "PRIVATEKEY: $PRIVATEKEY" >>/config_info.txt
-  echo "PUBLICKEY/PASSWORD: $PUBLICKEY" >>/config_info.txt
-  echo "NETWORK: $NETWORK" >>/config_info.txt
-  echo "XHTTP_PATH: $XHTTP_PATH" >>/config_info.txt
+  echo -e "\033[32m" >/config/config_info.txt
+  echo "IPV6: $IPV6" >>/config/config_info.txt
+  echo "IPV4: $IPV4" >>/config/config_info.txt
+  echo "UUID: $UUID" >>/config/config_info.txt
+  echo "DEST: $DEST" >>/config/config_info.txt
+  echo "PORT: $EXTERNAL_PORT" >>/config/config_info.txt
+  echo "SERVERNAMES: $SERVERNAMES (任选其一)" >>/config/config_info.txt
+  echo "PRIVATEKEY: $PRIVATEKEY" >>/config/config_info.txt
+  echo "PUBLICKEY/PASSWORD: $PUBLICKEY" >>/config/config_info.txt
+  echo "NETWORK: $NETWORK" >>/config/config_info.txt
+  echo "XHTTP_PATH: $XHTTP_PATH" >>/config/config_info.txt
 
   if [ "$IPV4" != "null" ]; then
     SUB_IPV4="vless://$UUID@$IPV4:$EXTERNAL_PORT?encryption=none&security=reality&type=$NETWORK&sni=$FIRST_SERVERNAME&fp=chrome&pbk=$PUBLICKEY&path=$XHTTP_PATH&mode=auto#${IPV4}-wulabing_docker_xhttp_reality"
-    echo "IPV4 订阅连接: $SUB_IPV4" >>/config_info.txt
-    echo -e "IPV4 订阅二维码:\n$(echo "$SUB_IPV4" | qrencode -o - -t UTF8)" >>/config_info.txt
+    echo "IPV4 订阅连接: $SUB_IPV4" >>/config/config_info.txt
+    echo -e "IPV4 订阅二维码:\n$(echo "$SUB_IPV4" | qrencode -o - -t UTF8)" >>/config/config_info.txt
   fi
   if [ "$IPV6" != "null" ];then
     SUB_IPV6="vless://$UUID@$IPV6:$EXTERNAL_PORT?encryption=none&security=reality&type=$NETWORK&sni=$FIRST_SERVERNAME&fp=chrome&pbk=$PUBLICKEY&path=$XHTTP_PATH&mode=auto#${IPV6}-wulabing_docker_xhttp_reality"
-    echo "IPV6 订阅连接: $SUB_IPV6" >>/config_info.txt
-    echo -e "IPV6 订阅二维码:\n$(echo "$SUB_IPV6" | qrencode -o - -t UTF8)" >>/config_info.txt
+    echo "IPV6 订阅连接: $SUB_IPV6" >>/config/config_info.txt
+    echo -e "IPV6 订阅二维码:\n$(echo "$SUB_IPV6" | qrencode -o - -t UTF8)" >>/config/config_info.txt
   fi
 
 
-  echo -e "\033[0m" >>/config_info.txt
+  echo -e "\033[0m" >>/config/config_info.txt
 
 fi
 
 # show config info
-cat /config_info.txt
+cat /config/config_info.txt
 
 # run xray
-exec /xray -config /config.json
+exec /xray -config /config/config.json
